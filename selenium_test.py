2212baa0a838a7703d4f79dfda1865aa4f08f985
csmith73 <cartersmith73@gmail.com>
2019-01-09 18:44:33 -0500

    Delete from cart when exceeded.

--- selenium_test.py
+++ selenium_test.py
@@ -5,7 +5,7 @@ chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox') # required when running as root user. otherwise you would get no sandbox errors.
 driver = webdriver.Chrome(executable_path='/dev/chromedriver', chrome_options=chrome_options,service_args=['--verbose', '--log-path=/tmp/chromedriver.log'])
 
-driver = webdriver.Chrome(executable_path='/dev/chromedriver',service_args=['--verbose', '--log-path=/tmp/chromedriver.log'])
+#driver = webdriver.Chrome(executable_path='/dev/chromedriver',service_args=['--verbose', '--log-path=/tmp/chromedriver.log'])
 # Log path added via service_args to see errors if something goes wrong (always a good idea - many of the errors I encountered were described in the logs)
 # And now you can add your website / app testing functionality:
 driver.get('https://python.org')
