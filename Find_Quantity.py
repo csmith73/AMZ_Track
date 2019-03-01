7ad9b64359a4a358648777142903e2c85c4c2aef
csmith73 <cartersmith73@gmail.com>
2019-02-28 22:07:58 -0500

    First working version max product 30

--- Find_Quantity.py
+++ Find_Quantity.py
@@ -74,6 +74,7 @@ def get_offer_id_list(asin):
     #chrome_options=options
     driver.get(offer_link)
     print(offer_link)
+    print(driver.page_source)
 
 
     #Detect pagination and call get_offerids_on_single_page() for each page.
